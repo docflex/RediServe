f51f61b9cb79bf4d9113c71e2702b089d7a4d513
Rehber Moin <rehbermoin.io@gmail.com>
2025-08-17 21:49:26 +0530

    Logger Fix

--- services/cache-gateway/src/main/java/com/cachegateway/service/CacheService.java
+++ services/cache-gateway/src/main/java/com/cachegateway/service/CacheService.java
@@ -1,20 +1,18 @@
 package com.cachegateway.service;
 
 import com.cachegateway.models.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
+@Slf4j
 @Service
 public class CacheService {
-
-    private static final Logger log = LoggerFactory.getLogger(CacheService.class);
 
     private final StringRedisTemplate redisTemplate;
     private final WebClient webClient;
